b12c57a4b8a6a3c6bd7f9909524fd9d8efecd68f
Shu <pas4o@yandex.ru>
2023-06-14 00:04:08 +0400

    Add Todo and Weather

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WeatherComponent} from "./components/weather/weather.component";
+import {TodoComponent} from "./components/todo/todo.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WeatherComponent},
+  {path: 'todo', component: TodoComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { TodoComponent } from './components/todo/todo.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WeatherComponent,
+    TodoComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
